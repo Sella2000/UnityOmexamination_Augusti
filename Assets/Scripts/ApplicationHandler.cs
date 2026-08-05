da8996a58df4c4684e9f19191877076745eb2652
Sella2000 <a25sebno@student.his.se>
2026-08-05 13:54:35 +0200

    StartScreenMedScenbyte

--- Assets/Scripts/ApplicationHandler.cs
+++ Assets/Scripts/ApplicationHandler.cs
@@ -9,8 +9,8 @@ public class ApplicationHandler : MonoBehaviour
 {
     public void ChangeScene()
     {
-        // Load the scene named "NewScene"
-        SceneManager.LoadSceneAsync("NewScene", LoadSceneMode.Single);
+        // Load the scene named "a25Sebno"
+        SceneManager.LoadSceneAsync("a25Sebno", LoadSceneMode.Single);
     }
 
     public void QuitGame()
